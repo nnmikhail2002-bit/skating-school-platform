378eff60a7091f14488bf2be6ee0ab29f29f8873
mikhail2222 <nnmikhail2002@gmail.com>
2026-09-16 19:35:58 +0300

    Create Spring Boot backend application

---
+++ backend/skating-platform-backend/src/main/java/com/skating/platform/backend/SkatingPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skating.platform.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkatingPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkatingPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ backend/skating-platform-backend/src/test/java/com/skating/platform/backend/SkatingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skating.platform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkatingPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
