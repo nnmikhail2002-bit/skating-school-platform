fa60f2348341d6a5295fe0f8ca79619a4533bae3
mikhail2222 <nnmikhail2002@gmail.com>
2026-09-25 16:44:55 +0300

    document service  with wagger

--- backend/skating-platform-backend/src/main/java/com/skating/platform/backend/controller/SchoolServiceController.java
+++ backend/skating-platform-backend/src/main/java/com/skating/platform/backend/controller/SchoolServiceController.java
@@ -4,6 +4,10 @@ import com.skating.platform.backend.dto.service.request.CreateSchoolServiceReque
 import com.skating.platform.backend.dto.service.request.UpdateSchoolServiceRequest;
 import com.skating.platform.backend.dto.service.response.SchoolServiceResponse;
 import com.skating.platform.backend.service.SchoolServiceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+
+@Tag(name = "Services", description = "Управление услугами школы")
 
 @RestController
 @RequestMapping("/api/services")
@@ -21,11 +27,23 @@ public class SchoolServiceController {
         this.schoolServiceService = schoolServiceService;
     }
 
+    @Operation(summary = "Получить все услуги", description =  "Выводит все услуги")
     @GetMapping
     public Page<SchoolServiceResponse> getAllServices(Pageable pageable){
         return schoolServiceService.getAllServices(pageable);
     }
 
+    @Operation(summary = "Создать услугу", description =  "Создает услугу")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Услуга создана"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректные данные услуги"
+            )
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SchoolServiceResponse createService(
@@ -34,22 +52,60 @@ public class SchoolServiceController {
         return schoolServiceService.createService(request);
     }
 
+    @Operation(summary = "Получить услугу по ID", description =  "Возвращает услугу по его идентификатору")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Услуга найдена"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Услуга не найдена"
+            )
+    })
     @GetMapping("/{serviceId}")
     public SchoolServiceResponse getServiceById(@PathVariable Long serviceId){
         return schoolServiceService.getServiceById(serviceId);
     }
 
+    @Operation(summary = "Обновить данные услуги по ID", description =  "Обновляет данные услуги")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "услуга найдена и успешно обновлена"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Некорректные данные для обновления услуги"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "услуга не найдена"
+            )
+    })
     @PutMapping("/{serviceId}")
     public SchoolServiceResponse updateService(@PathVariable Long serviceId, @Valid @RequestBody UpdateSchoolServiceRequest request){
         return schoolServiceService.updateService(serviceId, request);
     }
 
+    @Operation (summary = "Удалить услугу по ID", description =  "Удаляет услугу по её идентификатору")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Услуга успешно удалена"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "услуга не найдена"
+            )
+    })
     @DeleteMapping("/{serviceId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteService(@PathVariable Long serviceId){
         schoolServiceService.deleteService(serviceId);
     }
 
+    @Operation (summary = "Поиск услуг", description =  "Ищет услуги по названию или типу с поддержкой пагинации и сортировки")
     @GetMapping("/search")
     public Page<SchoolServiceResponse> searchServices(
             @RequestParam String query,
@@ -58,6 +114,7 @@ public class SchoolServiceController {
         return schoolServiceService.searchServices(query, pageable);
     }
 
+    @Operation (summary = "Фильтрация услуг", description =  "Фильтрует услуги по названию, типу, цене и активности с поддержкой пагинации и сортировки")
     @GetMapping("/filter")
     public Page<SchoolServiceResponse> filterServices(
             @RequestParam(required = false) String name,
